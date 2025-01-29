4746a262e5c2afce526c12040b4e3239559962d2
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-01-29 18:06:45 +0200

    added new test case for jest-test to check for invalid inputs during addition

--- jest_test/index.test.js
+++ jest_test/index.test.js
@@ -4,6 +4,10 @@ describe('Math Utilities Test Suite: add', () => {
 	it('Should add two integers', () => {
 		expect(add(2, 5)).toBe(7);
 	});
+
+	it("Should throw an error if we don't provide a valid number", () => {
+		expect(() => add('1', 2)).toThrow('Please provide a valid number.');
+	});
 });
 
 describe('Math Utilities Test Suite: multiply', () => {
