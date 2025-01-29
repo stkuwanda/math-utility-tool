74df415836f7c691acc5bb15a1450dde01936bac
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-01-29 14:31:01 +0200

    added package.json scripts to run test coverage for both native test tool and 3rd party jest tool, added minus tool to demo test coverage

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ function multiply(num1, num2) {
 	return num1 * num2;
 }
 
-export { add, multiply }; // named export
+function minus(num1, num2) {
+	return num1 - num2;
+}
+
+export { add, multiply, minus }; // named exports
